bdafaaeafe60ec7b31e1aa166ebddcf9ba443744
Francesca Matteu <francesca.matteu@itsincom.it>
2024-05-20 12:58:01 +0200

    Edited Order, IngredientType, Ingredient

--- PokeManagementDAL/Data/Ingredient.cs
+++ PokeManagementDAL/Data/Ingredient.cs
@@ -1,12 +1,23 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace PokeManagementDAL.Data
 {
-    internal class Ingredient
+    public  class Ingredient
     {
+        public int IngredientId { get; set; }
+        [MaxLength(50)]
+        public string Name { get; set; }   
+        public double AddictionalCost { get; set; }
+        [MaxLength(200)]
+        public string Description { get; set; }
+        public bool Allegergen { get; set; }
+        public double Calories {  get; set; }
+        public bool IsDeleted { get; set; }
+        public int Type {  get; set; } 
     }
 }
